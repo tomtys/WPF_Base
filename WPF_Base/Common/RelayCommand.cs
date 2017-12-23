0f79e1f956e18ac66ac59c88e731bf3fa9c86bc8
Tomasz Tyszka <tomasztyszka@gmail.com>
2017-12-23 07:14:29 +0100

    initial

---
+++ WPF_Base/Common/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Base.Common
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        // NOTE: add reference to PresentationCore 4.0.0
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+
+}
---
+++ WPF_Base/Models/Base.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Base.Models
+{
+    public abstract class Base : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // .NET 4.5
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void OnPropertyChanged<T>(Expression<Func<T>> expression)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(GetPropertyName(expression)));
+            }
+        }
+
+        private string GetPropertyName<T>(Expression<Func<T>> expression)
+        {
+            var member = expression.Body as MemberExpression;
+
+            if (member == null)
+                throw new ArgumentException();
+
+            return member.Member.Name;
+        }
+    }
+
+}
